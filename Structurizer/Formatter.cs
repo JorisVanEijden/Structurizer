b784ca1f82883eec81d3dae7861e9d6d7a49a42d
JvE <joris.vaneijden@gmail.com>
2024-06-15 12:14:39 +0200

    Add arrays to the formatter

--- Structurizer/Formatter.cs
+++ Structurizer/Formatter.cs
@@ -11,25 +11,50 @@ public class Formatter {
     private readonly Dictionary<string, EnumType>? _enums;
     private readonly Dictionary<string, StructType>? _structs;
     private readonly Dictionary<string, Variable>? _typeDefs;
-    
+
     public Formatter(StructurizerSettings config, StructureInformation? parseResult = null) {
         _structs = parseResult?.Structs;
         _enums = parseResult?.Enums;
         _typeDefs = parseResult?.TypeDefs.Any() is true ? parseResult.TypeDefs : config.TypeDefs;
     }
-    
+
     public virtual JsonNode Format(Variable variable, ReadOnlySpan<byte> bytes) {
+        // slice the bytes into variable.Count pieces of variable.Size bytes
+        var slices = new byte[variable.Count][];
+        for (var i = 0; i < variable.Count; i++) {
+            slices[i] = bytes.Slice(i * variable.Size, variable.Size).ToArray();
+        }
+
         if (_structs != null && _structs.TryGetValue(variable.Type, out StructType? structType)) {
+            if (variable.Count > 1) {
+                return new JsonArray(slices.Select(slice => FormatStruct(structType, slice) as JsonNode).ToArray());
+            }
+
             return FormatStruct(structType, bytes);
         }
-        
+
         if (_enums != null && _enums.TryGetValue(variable.Type, out EnumType? enumType)) {
+            if (variable.Count > 1) {
+                return new JsonArray(slices.Select(slice => FormatEnum(enumType, slice) as JsonNode).ToArray());
+            }
+
             return FormatEnum(enumType, bytes);
         }
-        
+
+        if (variable.Count > 1) {
+            var singleVariable = new Variable(variable.Name, variable.Type, variable.Size) {
+                Count = 1,
+                IsPointer = variable.IsPointer,
+                IsNear = variable.IsNear,
+                Alignment = variable.Alignment
+            };
+
+            return new JsonArray(slices.Select(slice => FormatType(singleVariable, slice) as JsonNode).ToArray());
+        }
+
         return FormatType(variable, bytes);
     }
-    
+
     private JsonValue FormatType(Variable variable, ReadOnlySpan<byte> bytes) {
         switch (variable.Type) {
             case "short" or "int":
@@ -47,14 +72,14 @@ public class Formatter {
             case {Type: "char", Count: 1}:
                 return JsonValue.Create(BitConverter.ToChar(bytes));
         }
-        
+
         if (_typeDefs != null && _typeDefs.TryGetValue(variable.Type, out Variable? typeDef)) {
             return Format(typeDef, bytes).AsValue();
         }
-        
+
         throw new NotSupportedException($"Type {variable.Type} not supported");
     }
-    
+
     private static JsonValue FormatEnum(EnumType enumType, ReadOnlySpan<byte> bytes) {
         uint value = enumType.MemberSize switch {
             1 => bytes[0],
@@ -62,21 +87,21 @@ public class Formatter {
             4 => BitConverter.ToUInt32(bytes),
             _ => throw new NotSupportedException($"Enum member size {enumType.MemberSize} not supported")
         };
-        
+
         if (enumType.Members.TryGetValue(value, out string name)) {
             return JsonValue.Create(name);
         }
-        
+
         throw new ArgumentOutOfRangeException(nameof(bytes), $"Enum value {value} not found in enum");
     }
-    
+
     public virtual JsonObject FormatStruct(StructType structType, ReadOnlySpan<byte> bytes) {
         if (bytes.Length < structType.Size) {
             throw new ArgumentOutOfRangeException(nameof(bytes), "Byte array is too small for the struct");
         }
-        
+
         var result = new JsonObject();
-        
+
         ReadOnlySpan<byte> structBytes = bytes[..structType.Size];
         var index = 0;
         foreach (Variable? member in structType.Members) {
@@ -85,7 +110,7 @@ public class Formatter {
             result.Add(member.Name, memberValue);
             index += member.Length;
         }
-        
+
         return result;
     }
 }
